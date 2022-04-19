30eca35d63b179976483366a10240150e25d50cf
dfpolo2022 <dfpolo@uninorte.edu.co>
2022-04-19 12:09:25 -0500

    Victory Screen

---
+++ Assets/Scripts/WinMessage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinMessage : MonoBehaviour
+{
+    public TextMeshProUGUI winMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("win") == 0)
+        {
+            winMessage.text = "PLAYER 2 WINS!";
+        }else if (PlayerPrefs.GetInt("win") == 1)
+        {
+            winMessage.text = "PLAYER 1 WINS!";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
